0000b70574513fde35d72e3f5428bee632804065
Alaminplabon <donplabon94@gmail.com>
2025-06-21 22:00:25 +0600

    schema

--- src/app/modules/invitation/invitation.models.ts
+++ src/app/modules/invitation/invitation.models.ts
@@ -4,6 +4,7 @@ import { Iinvitation } from './invitation.interface';
 const invitationSchema: Schema<Iinvitation> = new Schema(
   {
     email: { type: String, required: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   { timestamps: true },
 );
